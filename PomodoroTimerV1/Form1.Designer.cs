5b51f4cf94f922cb4eac3e886f791e4096a49dcc
s188691 <wiktor.wnekowicz@gmail.com>
2021-06-01 23:31:30 +0200

    Initial commit

---
+++ PomodoroTimerV1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace PomodoroTimerV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(320, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 67);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(286, 108);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(220, 33);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(369, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(341, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(164, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 39);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(528, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PomodoroTimerV1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PomodoroTimerV1
+{
+    public partial class Form1 : Form
+    {
+
+        public float workTime, breakTime;
+        public bool timerStarted = false;
+        public bool workOn = true;
+        public bool workOff = false;
+        public TimeSpan time;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            workTime = 10;
+            breakTime = 5;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TimerButton(timerStarted);
+            timerStarted = true;
+            ButtonsVisibility(true, false);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimerButton(timerStarted);
+            timerStarted = false;
+            ButtonsVisibility(false, true);
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timerStarted == true && workOn == true)
+            {
+                workTime--;
+                time = TimeSpan.FromSeconds(workTime);
+                label1.Text = time.ToString("mm':'ss");
+                label3.Text = "Work time";
+            } else if (timerStarted == true && workOff == true)
+            {
+                breakTime--;
+                time = TimeSpan.FromSeconds(breakTime);
+                label1.Text = breakTime.ToString("mm':'ss");
+                label3.Text = "Break time ^^";
+            }
+
+            if (breakTime == 0)
+            {
+                workOff = false;
+                workOn = true;
+                timer1.Stop();
+                timerStarted = false;
+                breakTime = 5;
+                label1.Text = workTime.ToString(); // To be changed
+                ButtonsVisibility(false, true); // To be changed
+            }
+
+            if (workTime == 0)
+            {
+                workOn = false;
+                workOff = true;
+                timer1.Stop();
+                timerStarted = false;
+                workTime = 10; //Reset time function to be replaced
+                label1.Text = breakTime.ToString();
+                ButtonsVisibility(false, true); // To be changed
+            }
+        }
+
+        public void TimerButton(bool button)
+        {
+            if (button == false)
+            {
+                timer1.Start();
+            }
+
+            if (button == true)
+            {
+                timer1.Stop();
+            }
+        }
+
+        public void ButtonsVisibility(bool one, bool two)
+        {
+            button1.Visible = one;
+            button2.Visible = two;
+        }
+    }
+}
